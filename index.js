5a5d64e732fa473f86f587bad9c3dc1c2eb58d2a
shezan <shezanahmedsourav223@gmail.com>
2025-08-29 17:06:22 +0600

    alert fixxed

--- index.js
+++ index.js
@@ -16,6 +16,47 @@ for(let heart of getHearts){
 let coin = 100 ;
 
 let  coinNumbers = document.getElementById("coin-numbers") ;
- let alertBtn= document.querySelectorAll("alert-btn") ;
+ let alertBtns= document.querySelectorAll(".alert-btn") ;
  let callHistory = document.getElementById("call-history") ;
 
+ for(const btn of alertBtns ){
+    btn.addEventListener('click', function(){
+      
+      
+
+       
+        if( coin < 20 ){
+            alert('Not Enough Coin')
+            return
+            
+        } 
+         coin -= 20 ;
+         coinNumbers.innerText =coin ;
+      let card =btn.closest('.card')
+
+      let serviceName = card.querySelector(".service-name").innerText;
+        let serviceNumber = card.querySelector(".service-number").innerText;
+        alert('service :' + serviceName)
+        alert('Number :' + serviceNumber)
+     
+
+   
+     
+       const li = document.createElement("li");
+    li.innerText = serviceName + " - " + serviceNumber;
+    callHistory.appendChild(li);
+    
+
+
+        
+         
+         
+         
+   
+         
+
+
+        
+        
+    })
+ }
